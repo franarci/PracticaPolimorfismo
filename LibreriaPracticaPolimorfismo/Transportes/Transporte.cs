35d6f2ba15a3b0baa20327c62568cb2bca74ebd4
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:52:28 -0300

    Creo constructor en Transporte

--- LibreriaPracticaPolimorfismo/Transportes/Transporte.cs
+++ LibreriaPracticaPolimorfismo/Transportes/Transporte.cs
@@ -8,9 +8,16 @@ namespace LibreriaPracticaPolimorfismo.Transportes
 {
     public abstract class Transporte
     {
+        public Transporte(int maxFuel, int maxNroPasajeros, int velocidad)
+        {
+            MaxFuel = maxFuel;
+            MaxNroDePasajeros = maxNroPasajeros;
+            Velocidad = velocidad;
+        }
+
         public int MaxFuel { get; set; }
         public int MaxNroDePasajeros { get; set; }
-        public double velocidad { get; set; }
+        public int Velocidad { get; set; }
 
         public abstract string acelerar();
         public abstract string desacelerar();
