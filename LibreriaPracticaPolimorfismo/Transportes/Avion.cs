925ea2216c2d0ddcbc82e5af7f1f7ea63b64d1ce
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:57:15 -0300

    agrego herencia en avion y constructor

--- LibreriaPracticaPolimorfismo/Transportes/Avion.cs
+++ LibreriaPracticaPolimorfismo/Transportes/Avion.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace LibreriaPracticaPolimorfismo.Transportes
 {
-    class Avion
+    public class Avion : Transporte
     {
+        public int MaxAltitud { get; set; }
+        public int NroDeMotores { get; set; }
+
+        public Avion(int maxFuel, int maxNroPasajeros, int velocidad, int maxAltitud, int nroMotores) : base(maxFuel,maxNroPasajeros,velocidad)
+        {
+            MaxAltitud = maxAltitud;
+            NroDeMotores = nroMotores;
+        }
+
+        public override string acelerar()
+        {
+            //TODO falta implementar acelerar() en avion
+            return ("acelerando avion");
+        }
+
+        public override string desacelerar()
+        {
+            //TODO falta implementar desacelerar() en avion
+            return ("desacelerando avion");
+        }
+
+        public string ascender()
+        {
+            //TODO falta implementar ascender()
+            return ("ascendiendo avion");
+        }
+        public string descender()
+        {
+            //TODO falta implementar descender()
+            return ("descendiendo avion");
+        }
+
     }
 }
