a32229abd85d6b62ec3a75022ae60250d88f1a14
franarci <fran.arciniega96@gmail.com>
2021-10-18 12:01:01 -0300

    agrego herencia a clase Auto y constructor

--- LibreriaPracticaPolimorfismo/Transportes/Auto.cs
+++ LibreriaPracticaPolimorfismo/Transportes/Auto.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace LibreriaPracticaPolimorfismo.Transportes
 {
-    class Auto
+    public class Auto : Transporte
     {
+        public string Modelo { get; set; }
+
+        public Auto(int maxFuel, int maxNroPasajeros, int velocidad, string modelo) : base(maxFuel, maxNroPasajeros, velocidad)
+        {
+            Modelo = modelo;
+        }
+
+        public override string acelerar()
+        {
+            //TODO falta implementar acelerar() en auto
+            return ("acelerando auto");
+        }
+
+        public override string desacelerar()
+        {
+            //TODO falta implementar desacelerar() en auto
+            return ("desacelerando auto");
+        }
+
+        public string doblarAIzq()
+        {
+            //TODO falta implementar doblarAIzq()
+            return ("doblando a izquierda");
+        }
+        public string doblarADer()
+        {
+            //TODO falta implementar doblarADer()
+            return ("doblando a derecha");
+        }
     }
 }
+
+    
