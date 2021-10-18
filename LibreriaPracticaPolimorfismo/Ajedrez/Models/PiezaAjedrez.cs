f1d45ddd46e6902bb4a703149d58033665e9b775
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:28:55 -0300

    Creo clase PiezaAjedrez

---
+++ LibreriaPracticaPolimorfismo/Ajedrez/Models/PiezaAjedrez.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Ajedrez.Models
+{
+    public abstract class PiezaAjedrez
+    {
+        public abstract string mover();
+    }
+}
