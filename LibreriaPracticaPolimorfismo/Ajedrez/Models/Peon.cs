70386a11f4fa585b313fb1a9696bb5b61d12a788
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:29:33 -0300

    Creo clase Peon

---
+++ LibreriaPracticaPolimorfismo/Ajedrez/Models/Peon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Ajedrez.Models
+{
+    public class Peon : PiezaAjedrez
+    {
+        public override string mover()
+        {
+            return "Peon solo avanza hacia adelante";
+        }
+    }
+}
