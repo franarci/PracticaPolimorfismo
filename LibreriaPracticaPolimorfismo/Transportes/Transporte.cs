d6ba062ec75c10f8980228b5819d65b33326916b
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:49:03 -0300

    Creo clase transporte

---
+++ LibreriaPracticaPolimorfismo/Transportes/Transporte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Transportes
+{
+    public abstract class Transporte
+    {
+        public int MaxFuel { get; set; }
+        public int MaxNroDePasajeros { get; set; }
+        public double velocidad { get; set; }
+
+        public abstract string acelerar();
+        public abstract string desacelerar();
+    }
+}
