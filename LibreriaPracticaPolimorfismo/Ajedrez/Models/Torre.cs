5ecd60bd19e1dfb3de66e639ddf5a33830d53911
franarci <fran.arciniega96@gmail.com>
2021-10-18 11:28:43 -0300

    Creo clase Torre

---
+++ LibreriaPracticaPolimorfismo/Ajedrez/Models/Torre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Ajedrez.Models
+{
+    public class Torre : PiezaAjedrez
+    {
+        public override string mover()
+        {
+            return "La torre mueve horizontal o verticalmente los espacios que quiera";
+        }
+    }
+}
