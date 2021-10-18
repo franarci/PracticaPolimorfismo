b5e88ffbacc671862410558c5acf87c67fe0f45e
franarci <fran.arciniega96@gmail.com>
2021-10-18 12:25:57 -0300

    Creo clase Animal

---
+++ LibreriaPracticaPolimorfismo/Animales/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Animales
+{
+    public abstract class Animal
+        
+    {
+        private int Edad;
+        private double Altura;
+        private double Peso;
+
+        public Animal(int edad, double altura, double peso)
+        {
+            Edad = edad;
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public int getEdad {
+            get
+            {
+                return Edad;
+            }
+        }
+        public double getAltura {
+            get
+            {
+                return Altura;
+            }
+        }
+        public double getPeso {
+            get
+            {
+                return Peso;
+            }
+        }
+    }
+}
