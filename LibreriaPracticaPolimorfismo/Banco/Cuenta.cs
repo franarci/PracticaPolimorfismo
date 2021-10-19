9ff4b9089c6c2637c7b9b563ced335bca5537b6a
franarci <fran.arciniega96@gmail.com>
2021-10-19 13:51:20 -0300

    Clase Cuenta

---
+++ LibreriaPracticaPolimorfismo/Banco/Cuenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Banco
+{
+    public abstract class Cuenta
+    {
+        public int IdCuenta { get; set; }
+        
+        public Cuenta(int idCuenta)
+        {
+            IdCuenta = idCuenta;
+        }
+        public abstract string calcularInteres();
+    }
+}
