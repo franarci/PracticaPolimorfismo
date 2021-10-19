785652acf795cce36493c52f6179ae884a174cb5
franarci <fran.arciniega96@gmail.com>
2021-10-19 13:51:43 -0300

    Clase CuentaCorriente

---
+++ LibreriaPracticaPolimorfismo/Banco/CuentaCorriente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Banco
+{
+    public class CuentaCorriente : Cuenta
+    {
+        public CuentaCorriente(int idCuenta) : base(idCuenta){}
+        public override string calcularInteres()
+        {
+            return "Calculando interes de cuenta corriente";
+        }
+    }
+}
