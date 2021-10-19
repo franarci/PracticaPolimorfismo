a12fc0765c807b9d30469a7eadc2345fa4cdec14
franarci <fran.arciniega96@gmail.com>
2021-10-19 13:51:29 -0300

    Clase CuentaAhorro

---
+++ LibreriaPracticaPolimorfismo/Banco/CuentaAhorro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Banco
+{
+    public class CuentaAhorro : Cuenta
+    {
+        public CuentaAhorro(int idCuenta) : base(idCuenta) { }
+        public override string calcularInteres()
+        {
+            return "Calculando interes de cuenta ahorro";
+        }
+    }
+}
