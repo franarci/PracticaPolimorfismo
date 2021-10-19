088ee221a1d9265f283c6c9b6d77b592745f0138
franarci <fran.arciniega96@gmail.com>
2021-10-18 22:48:25 -0300

    Creo clase Leon

---
+++ LibreriaPracticaPolimorfismo/Animales/Leon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Animales
+{
+    public class Leon : Animal
+    {
+        public double TamañoColmillos { get; set; }
+        public Leon(int edad, double altura, double peso, double tamColmillos) : base(edad,altura,peso)
+        {
+            TamañoColmillos = tamColmillos;
+        }
+        public override string imprimirComida()
+        {
+            return("Carne");
+        }
+    }
+}
