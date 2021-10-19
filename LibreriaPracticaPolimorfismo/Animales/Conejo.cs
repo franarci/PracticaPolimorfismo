0a86d58ab1688d99e541c23c930e457b02c69229
franarci <fran.arciniega96@gmail.com>
2021-10-18 22:50:57 -0300

    Creo clase Loro

---
+++ LibreriaPracticaPolimorfismo/Animales/Conejo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Animales
+{
+    public class Conejo : Animal
+    {
+        public double LongitudOrejas { get; set; }
+        public Conejo(int edad, double altura, double peso, double longOrejas) : base(edad, altura, peso)
+        {
+            LongitudOrejas = longOrejas;
+        }
+        public override string imprimirComida()
+        {
+            return ("Plantas");
+        }
+    }
+}
---
+++ LibreriaPracticaPolimorfismo/Animales/Loro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaPracticaPolimorfismo.Animales
+{
+    public class Loro : Animal
+    {
+        public double Coloracion { get; set; }
+        public Loro(int edad, double altura, double peso, double coloracion) : base(edad, altura, peso)
+        {
+            Coloracion = coloracion;
+        }
+        public override string imprimirComida()
+        {
+            return ("Frutas");
+        }
+    }
+}
